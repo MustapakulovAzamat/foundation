7e0ae3630faa34d2962c2659d561d0fb9deb268b
MustapakulovAzamat <azamustapakylov6@gmail.com>
2026-06-10 18:10:48 +0600

    add: create IngredientType interface for defining ingredient structure

---
+++ types/IngredientType.ts
@@ -0,0 +1,9 @@
+export interface IngredientType {
+  IngredientID: number;
+  ProductID: number
+  Quantity: number;
+  UnitPrice: number;
+  UnitName: string;
+  MaterialName: string;
+  ProductName: string;
+}
