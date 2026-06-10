3806dbe900dc886eed046e173bf7915e55271c6f
MustapakulovAzamat <azamustapakylov6@gmail.com>
2026-06-10 17:46:45 +0600

    api for gte employees

---
+++ app/api/employees/route.ts
@@ -0,0 +1,22 @@
+import {connectDB} from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const pool = await connectDB();
+    const result = await pool.request()
+      .query(`
+        SELECT EmployeeId, EmployeeName
+        FROM Employees
+      `);
+    return NextResponse.json({
+      success: true,
+      data: result.recordset
+    });
+  } catch {
+    return NextResponse.json({
+      success: false,
+      message: "Ошибка при получении сотрудников"
+    });
+  }
+}
