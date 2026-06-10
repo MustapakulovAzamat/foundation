5baebbfab5dc60d23440fed411cf76eb203afa0f
MustapakulovAzamat <azamustapakylov6@gmail.com>
2026-06-10 17:55:00 +0600

    api for get purchases history

---
+++ app/api/purchases/history/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "@/lib/db";
+
+export async function GET() {
+  try {
+    const pool = await connectDB()
+    const result = await pool.request()
+    .execute("GetPurchaseHistory");
+    return NextResponse.json({ 
+      success: true, 
+      data: result.recordset 
+    });
+  } catch {
+    return NextResponse.json({
+      success: false,
+      message: "Ошибка при загрузке истории закупок"
+    });
+  }
+}
